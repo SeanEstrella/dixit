80740c2e169ee73240b35dddb83a76d7f2d1e13a
migsaenz <saenzm4307@gmail.com>
2024-08-19 17:07:13 -0700

    storyteller logic theoretically done

--- storyteller.py
+++ storyteller.py
@@ -12,17 +12,19 @@ model, _, transform = open_clip.create_model_and_transforms(
 
 # makes the images (deck) array
 def load_images_from_directory(directory):
-    all_cards = []
+    initial_deck = []
     for filename in os.listdir(directory):
         if filename.endswith(('.png', '.jpg', '.jpeg')):
             name = os.path.join(directory, filename)
-            all_cards.append(name)
-    return all_cards
+            initial_deck.append(name)
+    return initial_deck
 
 def generate_description(imagePath):
     im = Image.open(imagePath).convert("RGB")
     im = transform(im).unsqueeze(0)
-    # query openclip with imagepath
+    # query openclip with Image itself
+    with torch.no_grad(), torch.cuda.amp.autocast():
+            generated = model.generate(im)
     description = (open_clip.decode(generated[0]).split("<end_of_text>")[0].replace("<start_of_text>", ""))
     return description
 
@@ -33,23 +35,27 @@ def main():
     all_cards = {}
     deck = load_images_from_directory(image_directory)
     hand = []
-    shuffled_deck = random.shuffle(deck)
-    for x in range(5):
-        
+    deck = random.shuffle(deck)
 
-    while(true):
+    # Initialize hand with size of 6 
+    for x in range(6):
+        hand.append(deck.pop())
+    
+    while (deck.length != 0):
+        # Select a card to play the role of the storyteller
+        hand = random.shuffle(hand)
+        current_card = hand.pop()
+        if current_card in all_cards:
+            current_description = all_cards[current_card]
 
-        # Generate descriptions for each image
-        with torch.no_grad(), torch.cuda.amp.autocast():
-            generated = model.generate(im)
+        else: 
+            current_description = generate_description(current_card)
+            all_cards[current_card] = current_description
 
-    
-        description = generate_description(path)
-    # Select a card to play the role of the storyteller
-    
-    
-    print(f"Storyteller selected the description for {selected_image}:")
-    print(storyteller_description)
+        print(f"Storyteller selected the following description for {current_card}:")
+        print(current_description)
+        hand.append(deck.pop())
+
 
 if __name__ == "__main__":
     main()
